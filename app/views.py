5b4490b631b4d4ff282f0f8e696eaca4bccb8a7e
Himmel König <itogreniyorum@gmail.com>
2021-10-13 22:56:56 +0300

    Static files added

--- app/views.py
+++ app/views.py
@@ -5,4 +5,10 @@ def home(request):
     # html = "<html><body>Hello World!</body></html>"
     # return HttpResponse(html)
     # print(request.path)
-    return HttpResponse('Hello Everybody...')
+    # return HttpResponse('Hello Everybody...')
+        context = {
+        'title': 'clarusway',
+        'dict1': {'django': 'best framework'},
+        'my_list': [2, 3, 4]
+    }
+        return render (request, 'app/home.html', context)
