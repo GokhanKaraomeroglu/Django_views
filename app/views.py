2f2dfe4982aa4c79448555f0285033662decb01f
Himmel König <itogreniyorum@gmail.com>
2021-10-13 22:04:39 +0300

    views changed

--- app/views.py
+++ app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 def home(request):
     # html = "<html><body>Hello World!</body></html>"
     # return HttpResponse(html)
+    # print(request.path)
     return HttpResponse('Hello Everybody...')
